ff748024e60a5971a87d86962f6fb47bcaedb1eb
Damon Hastings <damonjhastings@gmail.com>
2013-10-08 06:55:46 -0700

    Revert "Abspath for cforms"
    This reverts commit df7bb05db40619930b8911283b9b75c3a72a9d87.

--- wp-content/plugins/cforms/abspath.php
+++
@@ -1,7 +0,0 @@
-<?php 
-if (strpos($_SERVER['HTTP_HOST'], 'agdesigngroup.dev') !== false) {
-	$abspath = '/Users/damonhastings/AGD/AGD Site/'; 
-} else if (strpos($_SERVER['HTTP_HOST'], 'agdesigngroup.com') !== false) {
-	$abspath = '/var/www/vhosts/agdesigngroup.com/httpdocs/dev/'; 
-}
-?>
