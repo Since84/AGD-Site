c2e515651c02dd31be82d0d582a47464ebfcb8d5
Damon Hastings <damonjhastings@gmail.com>
2014-02-06 12:33:43 -0800

    Warning Page

--- wp-content/themes/AGDesign/parts/shared/html-header.php
+++ wp-content/themes/AGDesign/parts/shared/html-header.php
@@ -3,7 +3,6 @@
 <!--[if lt IE 7 ]><html class="no-js ie6" lang="en"><![endif]--> 
 <!--[if IE 7 ]><html class="no-js ie7" lang="en"><![endif]--> 
 <!--[if IE 8 ]><html class="no-js ie8" lang="en"><![endif]--> 
-<!--[if IE 8]><script src="js/browser-warning/warning.js"></script><script>window.onload=function(){e("js/browser-warning/")}</script><![endif]-->
 <!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" lang="en"><!--<![endif]-->
 	<head>
 		<title><?php bloginfo( 'name' ); ?><?php wp_title( '|' ); ?></title>
@@ -13,5 +12,9 @@
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
 		<?php wp_head(); ?>
+<!--[if IE 8 ]>
+		<link href="<?php bloginfo('template_url'); ?>/styles/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+		<script src="<?php bloginfo('template_url'); ?>/js/browser-warning/warning.js"></script><script>window.onload=function(){e("<?php bloginfo('template_url'); ?>/js/browser-warning/")}</script>
+<![endif]--> 
 	</head>
 	<body <?php body_class(); ?>>
